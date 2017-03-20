c0808fdb71118382dab980af0bd6ef388c8c2e1d
mordentral <morden@noxhub.net>
2017-03-20 10:20:34 -0400

    Updated to latest plugin ver

--- Source/VRExpPluginExample/EmptyClassForProject.h
+++ Source/VRExpPluginExample/EmptyClassForProject.h
@@ -1,6 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "VRCharacter.h"
+#include "GripMotionControllerComponent.h"
+#include "VRBPDatatypes.h"
+#include "VRGripInterface.h"
+#include "VRSimpleCharacter.h"
+#include "VRPlayerController.h"
+#include "VRExpansionFunctionLibrary.h"
+#include "GrippableBoxComponent.h"
 
 /**
  * 
@@ -10,4 +18,5 @@ class VREXPPLUGINEXAMPLE_API EmptyClassForProject
 public:
 	EmptyClassForProject();
 	~EmptyClassForProject();
+
 };
