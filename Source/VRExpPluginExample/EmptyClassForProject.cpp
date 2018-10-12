c22bd3158786b7b81157bb33174bf2d9c9a90c91
mordentral <morden@noxhub.net>
2018-10-12 14:59:49 -0400

    comp error

--- Source/VRExpPluginExample/EmptyClassForProject.cpp
+++ Source/VRExpPluginExample/EmptyClassForProject.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "VRExpPluginExample.h"
+//#include "VRExpPluginExample.h"
 #include "EmptyClassForProject.h"
 
 EmptyClassForProject::EmptyClassForProject()
