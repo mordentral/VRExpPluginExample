9a804b7ed6c3c038cbd115a99d0b9fee28169d62
mordentral <morden@noxhub.net>
2018-11-01 11:03:53 -0400

    making 4.19 work with vs 2017

--- Source/VRExpPluginExample/EmptyClassForProject.cpp
+++ Source/VRExpPluginExample/EmptyClassForProject.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VRExpPluginExample.h"
 #include "EmptyClassForProject.h"
+#include "VRExpPluginExample.h"
+
 
 EmptyClassForProject::EmptyClassForProject()
 {
