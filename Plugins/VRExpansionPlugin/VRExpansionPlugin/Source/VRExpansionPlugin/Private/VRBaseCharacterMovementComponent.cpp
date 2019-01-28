b3696c0d6a678471f606378dbf7cfc0d52f6ab80
mordentral <morden@noxhub.net>
2019-01-28 15:50:26 -0500

    testing a climbing / low grav change

--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRBaseCharacterMovementComponent.cpp
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRBaseCharacterMovementComponent.cpp
@@ -12,6 +12,8 @@
 #include "VRPlayerController.h"
 #include "GameFramework/PhysicsVolume.h"
 
+DECLARE_CYCLE_STAT(TEXT("ClimbingTick ~ Climbing"), STAT_TickClimb, STATGROUP_TickClimb);
+
 UVRBaseCharacterMovementComponent::UVRBaseCharacterMovementComponent(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
@@ -681,6 +683,15 @@ void UVRBaseCharacterMovementComponent::PhysCustom_Climbing(float deltaTime, int
 		return;
 	}
 
+	SCOPE_CYCLE_COUNTER(STAT_TickClimb);
+
+	// Allow the player to run updates on the climb logic for CustomVRInputVector
+	if (AVRBaseCharacter * characterOwner = Cast<AVRBaseCharacter>(CharacterOwner))
+	{
+		//SCOPE_CYCLE_COUNTER(STAT_TickClimb);
+		characterOwner->K2_UpdateClimbingMovement(deltaTime);
+	}
+
 	// I am forcing this to 0 to avoid some legacy velocity coming out of other movement modes, climbing should only be direct movement anyway.
 	Velocity = FVector::ZeroVector;
 
@@ -825,6 +836,12 @@ void UVRBaseCharacterMovementComponent::PhysCustom_LowGrav(float deltaTime, int3
 	if (deltaTime < MIN_TICK_TIME)
 	{
 		return;
+	}
+
+	// Allow the player to run updates on the push logic for CustomVRInputVector
+	if (AVRBaseCharacter * characterOwner = Cast<AVRBaseCharacter>(CharacterOwner))
+	{
+		characterOwner->K2_UpdateLowGravMovement(deltaTime);
 	}
 
 	float Friction = 0.0f; 
