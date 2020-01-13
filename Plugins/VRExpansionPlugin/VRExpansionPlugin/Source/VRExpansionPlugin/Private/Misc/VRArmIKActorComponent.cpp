4aeafd546738dc7ac16abd8f5723a39dca926dbd
morden <morden@noxhub.net>
2020-01-12 20:08:39 -0500

    added cpp

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/Misc/VRArmIKActorComponent.cpp
@@ -0,0 +1,39 @@
+#include "Misc/VRArmIKActorComponent.h"
+
+
+UVRArmIKActorComponent::UVRArmIKActorComponent(const FObjectInitializer& ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = true;
+	PrimaryComponentTick.TickGroup = TG_PostPhysics;
+	PrimaryComponentTick.bTickEvenWhenPaused = true;
+
+	referencePlayerHeightHmd = 1.94f * 100.f; // World To Meters idealy
+	referencePlayerWidthWrist = /*1.39f*/ 2.f * 100.f; // World To Meters idealy
+	playerHeightHmd = 1.94f * 100.f; // World To Meters idealy
+	playerWidthWrist = 1.39f/*2.f*/ * 100.f; // World To Meters idealy
+	playerWidthShoulders = 0.31f * 100.f; // World To Meters idealy
+
+	leftShoulderAnkerStartLocalPosition = FVector(0.f, -playerWidthShoulders / 2.f, 0.f);
+	rightShoulderAnkerStartLocalPosition = FVector(0.f, playerWidthShoulders / 2.f, 0.f);
+
+	loadPlayerSizeOnAwake = false;
+
+	headNeckDistance = 0.03f * 100.f; // World To Meters idealy
+	neckShoulderDistance = FVector(-0.02f, 0.f, -.1f) * 100.f; // World To Meters idealy
+
+	bEnableDistinctShoulderRotation = true;
+	distinctShoulderRotationMultiplier = 30.f;
+	distinctShoulderRotationLimitForward = 33.f;
+	distinctShoulderRotationLimitBackward = 0.f;
+	distinctShoulderRotationLimitUpward = 33.f;
+	bShoulderDislocated = false;
+	bEnableShoulderDislocation = false;
+	rightRotationStartHeight = 0.f;
+	rightRotationHeightFactor = 142.f;
+	rightRotationHeadRotationFactor = 0.3f;
+	rightRotationHeadRotationOffset = -20.f;
+	startShoulderDislocationBefore = 0.005f * 100.f; // World To Meters idealy
+	headNeckDirectionVector = FVector(-.05f, 0.f, -1.f);
+	bIgnoreZPos = true;
+}
--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Public/Misc/VRArmIKActorComponent.h
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Public/Misc/VRArmIKActorComponent.h
@@ -290,8 +290,8 @@ public:
 			
 			// Below still needs iteration, something is off with them
 			
-			//if (handSettings.rotateElbowWithHandRight)
-			//	rotateElbowWithHandRight();
+			if (handSettings.rotateElbowWithHandRight)
+				rotateElbowWithHandRight();
 			//if (handSettings.rotateElbowWithHandForward)
 			//	rotateElbowWithHandFoward();
 			//rotateHand();
@@ -530,7 +530,7 @@ public:
 		float deltaElbow = (elbowTargetAngle + (left ? -s.handDeltaOffset : s.handDeltaOffset)) / 180.f;
 		deltaElbow = FMath::Sign(deltaElbow) * FMath::Pow(FMath::Abs(deltaElbow), s.handDeltaPow) * 180.f * s.handDeltaFactor;
 
-		interpolatedDeltaElbow = LerpAxisOver(interpolatedDeltaElbow, deltaElbow, DeltaTime / s.rotateElbowWithHandDelay);
+		interpolatedDeltaElbow = deltaElbow;//LerpAxisOver(interpolatedDeltaElbow, deltaElbow, DeltaTime / s.rotateElbowWithHandDelay);
 		//interpolatedDeltaElbow = FRotator::ClampAxis(FMath::Lerp(interpolatedDeltaElbow, deltaElbow, FMath::Clamp(DeltaTime / s.rotateElbowWithHandDelay, 0.f, 1.f)));
 
 		rotateElbow(interpolatedDeltaElbow);
@@ -571,7 +571,7 @@ public:
 
 		deltaElbowForward = FMath::Sign(deltaElbowForward) * FMath::Pow(FMath::Abs(deltaElbowForward), s.handDeltaForwardPow) * 180.f;
 		
-		interpolatedDeltaElbowForward = LerpAxisOver(interpolatedDeltaElbowForward, deltaElbowForward, DeltaTime / s.rotateElbowWithHandDelay);
+		interpolatedDeltaElbowForward = deltaElbowForward;//LerpAxisOver(interpolatedDeltaElbowForward, deltaElbowForward, DeltaTime / s.rotateElbowWithHandDelay);
 		//interpolatedDeltaElbowForward = FRotator::ClampAxis(FMath::Lerp(interpolatedDeltaElbowForward, deltaElbowForward, DeltaTime / s.rotateElbowWithHandDelay));
 
 		float signedInterpolated = FRotator::NormalizeAxis(interpolatedDeltaElbowForward);
@@ -842,6 +842,8 @@ public:
 			CurrentTransforms.CameraTransform = HeadEff->GetComponentTransform() * ToLocalTrans;
 			CurrentTransforms.LeftHandTransform = LeftArmEff->GetComponentTransform() * ToLocalTrans;
 			CurrentTransforms.RightHandTransform = RightArmEff->GetComponentTransform() * ToLocalTrans;
+
+			DrawDebugLine(GetWorld(), CurrentTransforms.LeftHandTransform.GetLocation(), CurrentTransforms.LeftHandTransform.GetLocation() + CurrentTransforms.LeftHandTransform.GetRotation().GetRightVector() * 20.f, FColor::Cyan);
 			
 			/*CurrentTransforms.CameraTransform = OwningChar->VRReplicatedCamera->GetRelativeTransform();
 			CurrentTransforms.LeftHandTransform = OwningChar->LeftMotionController->GetRelativeTransform();
@@ -1170,40 +1172,3 @@ public:
 			shoulder.Transform.SetRotation(DeltaRot);
 		}
 };
-
-UVRArmIKActorComponent::UVRArmIKActorComponent(const FObjectInitializer& ObjectInitializer)
-{
-	PrimaryComponentTick.bCanEverTick = true;
-	PrimaryComponentTick.bStartWithTickEnabled = true;
-	PrimaryComponentTick.TickGroup = TG_PostPhysics;
-	PrimaryComponentTick.bTickEvenWhenPaused = true;
-
-	referencePlayerHeightHmd = 1.94f * 100.f; // World To Meters idealy
-	referencePlayerWidthWrist = /*1.39f*/ 2.f * 100.f; // World To Meters idealy
-	playerHeightHmd = 1.94f * 100.f; // World To Meters idealy
-	playerWidthWrist = 1.39f/*2.f*/ * 100.f; // World To Meters idealy
-	playerWidthShoulders = 0.31f * 100.f; // World To Meters idealy
-
-	leftShoulderAnkerStartLocalPosition = FVector(0.f, -playerWidthShoulders / 2.f, 0.f);
-	rightShoulderAnkerStartLocalPosition = FVector(0.f, playerWidthShoulders / 2.f, 0.f);
-
-	loadPlayerSizeOnAwake = false;
-
-	headNeckDistance = 0.03f * 100.f; // World To Meters idealy
-	neckShoulderDistance = FVector(-0.02f, 0.f, -.1f) * 100.f; // World To Meters idealy
-
-	bEnableDistinctShoulderRotation = true;
-	distinctShoulderRotationMultiplier = 30.f;
-	distinctShoulderRotationLimitForward = 33.f;
-	distinctShoulderRotationLimitBackward = 0.f;
-	distinctShoulderRotationLimitUpward = 33.f;
-	bShoulderDislocated = false;
-	bEnableShoulderDislocation = false;
-	rightRotationStartHeight = 0.f;
-	rightRotationHeightFactor = 142.f;
-	rightRotationHeadRotationFactor = 0.3f;
-	rightRotationHeadRotationOffset = -20.f;
-	startShoulderDislocationBefore = 0.005f * 100.f; // World To Meters idealy
-	headNeckDirectionVector = FVector(-.05f, 0.f, -1.f);
-	bIgnoreZPos = true;
-}
