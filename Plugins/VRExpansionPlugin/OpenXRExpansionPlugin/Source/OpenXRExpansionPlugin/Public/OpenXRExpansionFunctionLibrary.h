e2dc83457070b4d6afb8c20d2805527a11296cf7
mordentral <morden@noxhub.net>
2020-12-01 23:17:36 -0500

    working tracking system properties
    working tracking system properties, but not detailed enough, need to get access to the XrAction array so I can query the input source localized name.

--- Plugins/VRExpansionPlugin/OpenXRExpansionPlugin/Source/OpenXRExpansionPlugin/Public/OpenXRExpansionFunctionLibrary.h
+++ Plugins/VRExpansionPlugin/OpenXRExpansionPlugin/Source/OpenXRExpansionPlugin/Public/OpenXRExpansionFunctionLibrary.h
@@ -22,7 +22,7 @@ public:
 	~UOpenXRExpansionFunctionLibrary();
 public:
 
-	FOpenXRHMD* GetOpenXRHMD() const
+	static FOpenXRHMD* GetOpenXRHMD()
 	{
 		static FName SystemName(TEXT("OpenXR"));
 		if (GEngine->XRSystem.IsValid() && (GEngine->XRSystem->GetSystemName() == SystemName))
@@ -31,5 +31,51 @@ public:
 		}
 
 		return nullptr;
+	}
+
+
+	// Get a list of all currently tracked devices and their types, index in the array is their device index
+	UFUNCTION(BlueprintCallable, Category = "VRExpansionFunctions|OpenXR", meta = (bIgnoreSelf = "true"))
+		static bool GetXRTrackingSystemProperties(int32 & VendorID, FString & SystemName)
+	{
+		if (FOpenXRHMD* pOpenXRHMD = GetOpenXRHMD())
+		{
+			XrInstance XRInstance = pOpenXRHMD->GetInstance();
+			XrSystemId XRSysID = pOpenXRHMD->GetSystem();
+
+			if (XRSysID && XRInstance)
+			{
+				XrSystemProperties systemProperties{ XR_TYPE_SYSTEM_PROPERTIES };
+				systemProperties.next = nullptr;
+				//XR_ENSURE(xrGetSystemProperties(XRInstance, XRSysID, &systemProperties));
+
+				if (xrGetSystemProperties(XRInstance, XRSysID, &systemProperties) == XR_SUCCESS)
+				{
+					
+
+					/*XrInputSourceLocalizedNameGetInfo InputSourceInfo{ XR_TYPE_INPUT_SOURCE_LOCALIZED_NAME_GET_INFO };
+					InputSourceInfo.next = nullptr;
+					InputSourceInfo.whichComponents = XR_INPUT_SOURCE_LOCALIZED_NAME_INTERACTION_PROFILE_BIT | XR_INPUT_SOURCE_LOCALIZED_NAME_USER_PATH_BIT | XR_INPUT_SOURCE_LOCALIZED_NAME_COMPONENT_BIT;
+
+					XrPath myPath;
+					XrResult PathResult = xrStringToPath(XRInstance, "/user/hand/left", &myPath);
+					InputSourceInfo.sourcePath = myPath;
+					XrSession XRSesh = pOpenXRHMD->GetSession();
+					char buffer[XR_MAX_SYSTEM_NAME_SIZE];
+					uint32_t usedBufferCount = 0;
+					if (xrGetInputSourceLocalizedName(XRSesh, &InputSourceInfo, XR_MAX_SYSTEM_NAME_SIZE, &usedBufferCount, (char*)&buffer) == XR_SUCCESS)
+					{
+						int gg = 0;
+					}*/
+
+					SystemName = FString(ANSI_TO_TCHAR(systemProperties.systemName));// , XR_MAX_SYSTEM_NAME_SIZE);
+					VendorID = systemProperties.vendorId;
+					return true;
+				}
+			}
+		}
+
+		SystemName.Empty();
+		return false;
 	}
 };	
