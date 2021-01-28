e4dad3df10e3c67e9c0af356b80044635f62e26b
mordentral <morden@noxhub.net>
2021-01-27 21:16:01 -0500

    finished modifications
    finished modifications

--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripScripts/GS_Melee.cpp
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripScripts/GS_Melee.cpp
@@ -873,53 +873,8 @@ bool UGS_Melee::GetWorldTransform_Implementation
 			{
 				//FVector curLocation; // Current location of the secondary grip
 				
-				bool bPulledControllerLoc = false;
-				if (UGripMotionControllerComponent* OtherController = Cast<UGripMotionControllerComponent>(Grip.SecondaryGripInfo.SecondaryAttachment))
-				{
-					bool bPulledCurrentTransform = false;
-
-					if (OtherController->CustomPivotComponent.IsValid())
-					{
-						FTransform SecondaryTrans = FTransform::Identity;
-						SecondaryTrans = OtherController->GetPivotTransform();
-						bPulledControllerLoc = true;
-
-						if (OtherController->CustomPivotComponent->GetAttachParent() == OtherController)
-						{
-							if (GrippingController->bHasAuthority && Grip.SecondaryGripInfo.SecondaryAttachment->GetOwner() == GrippingController->GetOwner() && !OtherController->bUseWithoutTracking)
-							{
-								FVector Position = FVector::ZeroVector;
-								FRotator Orientation = FRotator::ZeroRotator;
-								float WorldToMeters = GetWorld() ? GetWorld()->GetWorldSettings()->WorldToMeters : 100.0f;
-								if (OtherController->GripPollControllerState(Position, Orientation, WorldToMeters))
-								{
-									// If we are the local player lets avoid tick ordering issues by updating the pivot
-									FTransform ControllerTrans = OtherController->GetComponentTransform();
-									SecondaryTrans = OtherController->CalcControllerComponentToWorld(Orientation, Position) * SecondaryTrans.GetRelativeTransform(ControllerTrans);
-								}
-							}
-						}
-
-						frontLoc = SecondaryTrans.GetLocation() - BasePoint;
-					}
-					else
-					{
-						if (GrippingController->bHasAuthority && Grip.SecondaryGripInfo.SecondaryAttachment->GetOwner() == GrippingController->GetOwner() && !OtherController->bUseWithoutTracking)
-						{
-							bPulledControllerLoc = true;
-							FVector Position = FVector::ZeroVector;
-							FRotator Orientation = FRotator::ZeroRotator;
-							float WorldToMeters = GetWorld() ? GetWorld()->GetWorldSettings()->WorldToMeters : 100.0f;
-							if (OtherController->GripPollControllerState(Position, Orientation, WorldToMeters))
-							{
-								frontLoc = OtherController->CalcControllerComponentToWorld(Orientation, Position).GetLocation() - BasePoint;
-							}
-						}
-					}
-				}
-
-				if (!bPulledControllerLoc)
-					/*curLocation*/ frontLoc = Grip.SecondaryGripInfo.SecondaryAttachment->GetComponentLocation() - BasePoint;
+				// Calculates the correct secondary attachment location and sets frontLoc to it
+				CalculateSecondaryLocation(frontLoc, BasePoint, Grip, GrippingController);
 
 				frontLocOrig = (/*WorldTransform*/SecondaryTransform.TransformPosition(Grip.SecondaryGripInfo.SecondaryRelativeTransform.GetLocation())) - BasePoint;
 
