3eab18ba9d05ecb203e3b26c383bcb78b44d76b3
Joshua <mordentral@gmail.com>
2022-04-18 15:24:32 -0400

    collision ignore cleanups and bug fixes.
    need to fully test it but taking it live on the template until then

--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/Misc/CollisionIgnoreSubsystem.cpp
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/Misc/CollisionIgnoreSubsystem.cpp
@@ -17,43 +17,61 @@ DEFINE_LOG_CATEGORY(VRE_CollisionIgnoreLog);
 
 void UCollisionIgnoreSubsystem::CheckActiveFilters()
 {
-
-	bool bDoubleCheckPairs = false;
-	for (const TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : CollisionTrackedPairs)
+	for (TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : CollisionTrackedPairs)
 	{
 		// First check for invalid primitives
-		if (!IsValid(KeyPair.Key.Prim1) || !IsValid(KeyPair.Key.Prim2) /*|| KeyPair.Key.Prim1->IsPendingKill() || KeyPair.Key.Prim2->IsPendingKill()*/)
+		if (!IsValid(KeyPair.Key.Prim1) || !IsValid(KeyPair.Key.Prim2))
 		{
 			// If we don't have a map element for this pair, then add it now
 			if (!RemovedPairs.Contains(KeyPair.Key))
 			{
 				RemovedPairs.Add(KeyPair.Key, KeyPair.Value);
 			}
-			/*else
-			{
-				RemovedPairs[KeyPair.Key].PairArray.AddUnique(KeyPair.Value);
-			}*/
 
 			continue; // skip remaining checks as we have invalid primitives anyway
 		}
 
 		// Now check for lost physics handles
 		// Implement later
-		/*for (const FCollisionIgnorePair& IgnorePair : KeyPair.Value.PairArray)
+
+
+		for (int i = KeyPair.Value.PairArray.Num() - 1; i >= 0; i--)
 		{
 			if (
-				(!IgnorePair.Actor1.IsValid() || !FPhysicsInterface::IsValid(IgnorePair.Actor1)) ||
-				(!IgnorePair.Actor2.IsValid() || !FPhysicsInterface::IsValid(IgnorePair.Actor2))
+				(!KeyPair.Value.PairArray[i].Actor1 || !FPhysicsInterface::IsValid(KeyPair.Value.PairArray[i].Actor1)) ||
+				(!KeyPair.Value.PairArray[i].Actor2 || !FPhysicsInterface::IsValid(KeyPair.Value.PairArray[i].Actor2))
 				)
 			{
-				// Reinit the ignoring, one of the handles got lost
-				// TODO
-				//
-				InitiateIgnore();
 
-				bDoubleCheckPairs = true;
+				FName Bone1 = KeyPair.Value.PairArray[i].BoneName1;
+				FName Bone2 = KeyPair.Value.PairArray[i].BoneName2;
+
+				FBodyInstance* Inst1 = KeyPair.Key.Prim1->GetBodyInstance(Bone1);
+				FBodyInstance* Inst2 = KeyPair.Key.Prim2->GetBodyInstance(Bone2);
+
+				if (Inst1 && Inst2)
+				{
+					// We still have the bones available, lets go ahead and re-init for them
+					KeyPair.Value.PairArray.RemoveAt(i);
+					SetComponentCollisionIgnoreState(false, false, KeyPair.Key.Prim1, Bone1, KeyPair.Key.Prim2, Bone2, true, false);
+				}
+				else
+				{
+					// Its not still available, lets remove the pair.
+					KeyPair.Value.PairArray.RemoveAt(i);
+				}
 			}
-		}*/
+		}
+
+		// If there are no pairs left
+		if (KeyPair.Value.PairArray.Num() < 1)
+		{
+			// Try and remove it, chaos should be cleaning up the ignore setups
+			if (!RemovedPairs.Contains(KeyPair.Key))
+			{
+				RemovedPairs.Add(KeyPair.Key, KeyPair.Value);
+			}
+		}
 	}
 
 #if WITH_CHAOS
@@ -70,16 +88,6 @@ void UCollisionIgnoreSubsystem::CheckActiveFilters()
 	}*/
 #endif
 
-	if (RemovedPairs.Num() > 0 || bDoubleCheckPairs == true)
-	{
-		if (FPhysScene* PhysScene = GetWorld()->GetPhysicsScene())
-		{
-#if WITH_CHAOS
-			// I think chaos may handle this itself here, i need to double check
-#endif
-		}
-	}
-
 	for (const TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : RemovedPairs)
 	{
 		if (CollisionTrackedPairs.Contains(KeyPair.Key))
@@ -88,8 +96,6 @@ void UCollisionIgnoreSubsystem::CheckActiveFilters()
 			CollisionTrackedPairs.Remove(KeyPair.Key);
 		}
 	}
-
-	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("NumIgnored Actors: %i"), CollisionTrackedPairs.Num()));
 
 	UpdateTimer();
 }
@@ -113,20 +119,17 @@ void UCollisionIgnoreSubsystem::RemoveComponentCollisionIgnoreState(UPrimitiveCo
 		}
 	}
 
-	if (RemovedPairs.Num() > 0)
-	{
-		if (FPhysScene* PhysScene = GetWorld()->GetPhysicsScene())
-		{
-#if WITH_CHAOS
-			// I think chaos may handle this itself here, i need to double check
-#endif
-		}
-	}
-
 	for (const TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : RemovedPairs)
 	{
 		if (CollisionTrackedPairs.Contains(KeyPair.Key))
 		{
+			for (const FCollisionIgnorePair& newIgnorePair : KeyPair.Value.PairArray)
+			{
+				// Clear out current ignores
+				SetComponentCollisionIgnoreState(false, false, KeyPair.Key.Prim1, newIgnorePair.BoneName1, KeyPair.Key.Prim2, newIgnorePair.BoneName2, false, false);
+			}
+
+			// Ensure we are empty
 			CollisionTrackedPairs[KeyPair.Key].PairArray.Empty();
 			CollisionTrackedPairs.Remove(KeyPair.Key);
 		}
@@ -276,12 +279,6 @@ void UCollisionIgnoreSubsystem::SetComponentCollisionIgnoreState(bool bIterateCh
 			{
 				if (FPhysScene* PhysScene = Prim1->GetWorld()->GetPhysicsScene())
 				{
-					FContactModBodyInstancePair newContactPair;
-					newContactPair.Actor1 = ApplicableBodies[i].BInstance->ActorHandle;
-					newContactPair.Actor2 = ApplicableBodies2[j].BInstance->ActorHandle;
-					newContactPair.Prim1 = Prim1;
-					newContactPair.Prim2 = Prim2;
-
 					FCollisionIgnorePair newIgnorePair;
 					newIgnorePair.Actor1 = ApplicableBodies[i].BInstance->ActorHandle;
 					newIgnorePair.BoneName1 = ApplicableBodies[i].BName;
@@ -313,8 +310,20 @@ void UCollisionIgnoreSubsystem::SetComponentCollisionIgnoreState(bool bIterateCh
 										ParticleHandle1->AddCollisionConstraintFlag(Chaos::ECollisionConstraintFlags::CCF_BroadPhaseIgnoreCollisions);
 										IgnoreCollisionManager.AddIgnoreCollisionsFor(ID1, ID0);
 
-										if (CollisionTrackedPairs.Contains(newPrimPair))
+
+										TSet<FCollisionPrimPair> CurrentKeys;
+										int numKeys = CollisionTrackedPairs.GetKeys(CurrentKeys);
+
+										// This checks if we exist already as well as provides an index
+										if (FCollisionPrimPair* CurrentPair = CurrentKeys.Find(newPrimPair))
 										{
+											// Check if the current one has the same primitive ordering as the new check
+											if (CurrentPair->Prim1 != newPrimPair.Prim1)
+											{
+												// If not then lets flip the elements around in order to match it
+												newIgnorePair.FlipElements();
+											}
+
 											CollisionTrackedPairs[newPrimPair].PairArray.AddUnique(newIgnorePair);
 										}
 
